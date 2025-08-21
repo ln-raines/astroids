2486ebb1ca1c606728164697e97a5d8b82c6f58f
ln-raines <lydia.raines@gmail.com>
2025-08-21 14:20:42 -0400

    Adding player sprite

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import pygame
-from constants import * 
+from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    # Game loop
+    # Game Loop Starts Here:
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
-    
-    
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
