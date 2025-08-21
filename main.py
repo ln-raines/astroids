ec3c8fa8864eb830c511a80e15e119ac62b5226e
ln-raines <lydia.raines@gmail.com>
2025-08-21 12:29:03 -0400

    Added FPS

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")   # correct way in pygame
+        screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
     
     
